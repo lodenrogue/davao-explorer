531a04aedb88995a332e21a6004859e49f6a8109
lodenrogue <phyzix5761@gmail.com>
2026-05-10 03:05:18 +0800

    Changed the colors of different nodes. Changed legend items

--- src/categories.js
+++ src/categories.js
@@ -1,8 +1,8 @@
 const CATEGORY_COLORS = {
     attraction: "#00d9ff",
     restaurant: "#ff2fd1",
-    park: "#ff2fd1",
-    market: "#ff2fd1",
-    mall: "#00ffa3",
+    park: "#00ffa3",
+    market: "#ff9f43",
+    mall: "#ff2fd1",
     cafe: "#ff9f43"
 };
